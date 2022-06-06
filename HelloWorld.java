931bedb390794b2eaf1a988a9def9f7bac1ecff7
dulceld <99284592+dulceld@users.noreply.github.com>
2022-06-06 10:32:02 -0700

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,21 @@
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
+import javax.swing.JOptionPane;
+ 
+public class Helloworld {
+public static void main(String[] args) { 
+
+String a = "What is your name? ";
+String b = "How are you? ";
+String c = "What is your favoite food? ";
+
+String d = JOptionPane.showInputDialog("What is your name?");
+String e = JOptionPane.showInputDialog("How are you?");
+String f = JOptionPane.showInputDialog("What is your favorite food?");
+
+System.out.println("Hello!");
+System.out.println(a + d);
+System.out.println(b + e);
+System.out.println(c + f);  
+System.out.println("Very nice!");
+
+}
 }
